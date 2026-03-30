7384b8fc54a4323727823b55befba8f988996a8d
Clément GUICHARD <clement.guichard@csgroup.eu>
2026-03-30 12:07:18 +0200

    refactor: update copyright headers

--- src/eoap_tools/_cli.py
+++ src/eoap_tools/_cli.py
@@ -1,4 +1,4 @@
-# Copyright 2025, CS GROUP - France, https://www.csgroup.eu/
+# Copyright 2026, CS GROUP - France, https://www.csgroup.eu/
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
